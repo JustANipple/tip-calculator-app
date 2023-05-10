441689930f010bb57c3b03638c2229c79804c431
JustANipple <prandisamuel99@gmail.com>
2023-05-10 16:39:55 +0200

    modified:   index.html 	modified:   scripts/scripts.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/component/_tipCalculator.scss 	modified:   styles/scss/component/_tipResult.scss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,134 +32,45 @@ Amounts states:
 -Amounts update if bill, any tip and people number is compiled and valid
 */
 
-/* === Bill and People number Input === */
-const inputElems = document.querySelectorAll(".compile_input");
-//if you need to pass an argument to the function you're calling, use the arrow function so that the function doesn't get instantly called
-for(const elem of inputElems) {
-    elem.addEventListener("input", () => {
-        checkInput(elem);
-        checkIfEmpty();
-    });
+/*
+bill, custom and people patterns
+Bill: decimal, not 0, not empty
+Custom: int, from 1 to 100, not empty
+People: int, not 0, not empty
+*/
+
+/* Variables */
+let billAndPeople = document.querySelectorAll(".compile_input");
+const customTip = document.querySelector(".tip_custom");
+const tipBtns = document.querySelectorAll(".tip_percentage"); //tip
+const amountTip = document.querySelector(".tip_value .value_number"); //tip amount
+const amountTot = document.querySelector(".total_value .value_number"); //total
+const resetBtn = document.querySelector(".reset"); //reset
+
+/* Events */
+for(const input of billAndPeople) {
+    input.addEventListener("input", checkInput);
 }
-function checkInput(obj) {
-    const errorLabel = obj.parentElement.parentElement.querySelector(".label_error");
-    obj.parentElement.style.transition = "all .5s ease-in-out";
-    errorLabel.style.transition = "all .5s ease-in-out";
-    //to check if element is valid, just pass the element, not his value
-    if(obj.checkValidity() && obj.value !== "") {
-        obj.parentElement.setAttribute("id", "inputValid");
-        errorLabel.style.visibility = "hidden";
-        errorLabel.style.opacity = "0";
+
+function checkInput() {
+    const error = this.parentElement.parentElement.querySelector(".label_error");
+    const cont = this.parentElement;
+    if(this.value < 0) {
+        this.setCustomValidity("must be greater than 0");
+        cont.setAttribute("id", "error");
+    } else if (this.value === "0") {
+        cont.setAttribute("id", "error");
+        error.style.opacity = "1";
+        error.style.visibility = "visible";
+    } else if (this.value === "") {
+        this.setCustomValidity("compile this");
+        cont.setAttribute("id", "");
+        error.style.opacity = "0";
+        error.style.visibility = "hidden";
     } else {
-        obj.parentElement.setAttribute("id", "inputInvalid");
-        errorLabel.style.visibility = "visible";
-        errorLabel.style.opacity = "1";
-    }
-}
-
-/* === Tip percentage selection, validation === */
-/* Percentage buttons */
-const percentageElems = document.querySelectorAll(".tip_percentage");
-for(const percentage of percentageElems) {
-    percentage.style.transition = "all .5s ease-in-out";
-    percentage.addEventListener("click", () => {
-        changeStatus(percentage);
-        checkIfEmpty();
-    });
-    percentage.addEventListener("mouseover", () => {
-        percentage.style.filter = "saturate(.375) brightness(1.375)";
-        
-    });
-    percentage.addEventListener("mouseout", () => {
-        percentage.style.filter = "saturate(1) brightness(1)";
-    });
-}
-
-let chosen;
-function changeStatus(obj) {
-    obj.setAttribute("id", "chosen");
-    if(chosen === obj) {
-        chosen = undefined;
-        obj.setAttribute("id", "");
-    } else if(chosen === undefined) {
-        chosen = obj;
-    } else {
-        chosen.setAttribute("id", "");
-        chosen = obj;
-    }
-}
-/* Percentage custom input */
-const customPerc = document.querySelector(".tip_custom");
-customPerc.addEventListener("input", () => {
-    checkCustomPerc(customPerc);
-    checkIfEmpty();
-});
-
-function checkCustomPerc(obj) {
-    obj.style.transition = "all .5s ease-in-out";
-    //to check if element is valid, just pass the element, not his value
-    if(obj.checkValidity() && obj.value !== "0" && obj.value !== "") {
-        obj.setAttribute("id", "inputValid");
-    } else if (obj.value === "") {
-        obj.setAttribute("id", "");
-    } else {
-        obj.setAttribute("id", "inputInvalid");
-    }
-}
-
-/* === Reset button === */
-const resetBtn = document.querySelector(".reset");
-/* Checking: bill input, numer of people input, buttons checked, custom input */
-resetBtn.addEventListener("click", () => {
-    for(const elem of inputElems) {
-        elem.value = "";
-        elem.parentElement.setAttribute("id", "");
-    }
-    for(const elem of percentageElems) {
-        elem.setAttribute("id", "");
-    }
-    customPerc.value = "";
-    customPerc.setAttribute("id", "");
-    resetBtn.disabled = true;
-});
-
-function checkIfEmpty() {
-    let isEmpty = true;
-    for(const elem of inputElems) {
-        if(elem.value !== "") {
-            isEmpty = false;
-        }
-    }
-    for(const elem of percentageElems) {
-        if(elem.getAttribute("id") === "chosen") {
-            isEmpty = false;
-        }
-    }
-    if(customPerc.value !== "") {
-        isEmpty = false;
-    }
-    if(isEmpty) {
-        resetBtn.disabled = true;
-    } else {
-        resetBtn.disabled = false;
-    }
-}
-
-resetBtn.addEventListener("mouseover", () => {
-    resetBtn.style.filter = "saturate(.375) brightness(1.375)";
-    
-});
-resetBtn.addEventListener("mouseout", () => {
-    resetBtn.style.filter = "saturate(1) brightness(1)";
-});
-
-/* === Amounts === */
-const amounts = document.querySelectorAll(".value_number");
-
-function calculateAmounts() {
-    const tipPerc = chosen.textContent.replace("%", "");
-    const tipAmount = inputElems[0] / 100 * parseInt(tipPerc);
-    if(tipAmount > 0) {
-        amounts[0].textContent = tipAmount;
+        this.setCustomValidity("");
+        cont.setAttribute("id", "valid");  
+        error.style.opacity = "0";
+        error.style.visibility = "hidden";
     }
 }
