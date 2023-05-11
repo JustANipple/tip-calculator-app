94cabd7d91a320c5941e01273b1ce7704c1cfed6
JustANipple <prandisamuel99@gmail.com>
2023-05-11 10:35:36 +0200

    modified:   index.html 	modified:   scripts/scripts.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/component/_tipResult.scss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -40,7 +40,7 @@ People: int, not 0, not empty
 */
 
 /* Variables */
-let billAndPeople = document.querySelectorAll(".compile_input"); //bill and people inputs
+const billAndPeople = document.querySelectorAll(".compile_input"); //bill and people inputs
 const customTip = document.querySelector(".tip_custom"); //custom tip
 const tipBtns = document.querySelectorAll(".tip_percentage"); //tip
 const amountTip = document.querySelector(".tip_value .value_number"); //tip amount
@@ -49,49 +49,75 @@ const resetBtn = document.querySelector(".reset"); //reset
 
 /* Events */
 for(const input of billAndPeople) {
-    input.addEventListener("input", checkInput);
-}
-customTip.addEventListener("input", checkInput);
-
-customTip.addEventListener("input", resetButtons);
-for(const btn of tipBtns) {
-    btn.addEventListener("click", resetCustom);
-    btn.addEventListener("click", (e) => {
-        changeStatus(btn, e);
+    input.addEventListener("input", () => {
+        checkInput(input);
+        enableReset();
+        calculateAmount();
     });
 }
 
+customTip.addEventListener("input", () => {
+    checkInput(customTip);
+    resetButtons();
+    enableReset();
+    calculateAmount();
+})
+
+for(const btn of tipBtns) {
+    btn.style.transition = "filter .5s ease-in-out";
+    btn.addEventListener("click", (e) => {
+        changeStatus(btn, e);
+        resetCustom();
+        enableReset();
+        calculateAmount();
+    });
+}
+
+resetBtn.addEventListener("click", resetForm);
+resetBtn.style.transition = "filter .5s ease-in-out";
+
 
 /* Functions */
-function checkInput() {
-    const error = this.parentElement.parentElement.querySelector(".label_error");
-    let cont = this.parentElement;
+function checkInput(obj) {
+    const error = obj.parentElement.parentElement.querySelector(".label_error");
+    let cont = obj.parentElement;
     if(!error) {
-        cont = this;
+        cont = obj;
     }
-    if(this.value < 0) {
-        this.setCustomValidity("must be greater than 0");
+    if(obj.value < 0) {
+        obj.setCustomValidity("must be greater than 0");
         cont.setAttribute("id", "error");
-    } else if (this.value === "0") {
+    } else if (obj.value === "0") {
         cont.setAttribute("id", "error");
         if(error) {
             error.style.opacity = "1";
             error.style.visibility = "visible";
         }
-    } else if (this.value === "") {
-        this.setCustomValidity("compile this");
+    } else if (obj.value === "") {
+        obj.setCustomValidity("compile this");
         cont.setAttribute("id", "");
         if(error) {
             error.style.opacity = "0";
             error.style.visibility = "hidden";
         }
     } else {
-        this.setCustomValidity("");
+        obj.setCustomValidity("");
         cont.setAttribute("id", "valid");  
         if(error) {
             error.style.opacity = "0";
             error.style.visibility = "hidden";
         }
+    }
+}
+
+function resetInputs() {
+    for(const input of billAndPeople) {
+        const error = input.parentElement.parentElement.querySelector(".label_error");
+        const inCont = input.parentElement;
+        inCont.setAttribute("id", "");
+        input.value = "";
+        error.style.visibility = "hidden";
+        error.style.opacity = "0";
     }
 }
 
@@ -110,4 +136,19 @@ function changeStatus(btn, e) {
     e.preventDefault();
     resetButtons();
     btn.setAttribute("id", "chosen");
+}
+
+function enableReset() {
+    resetBtn.disabled = false;
+}
+
+function resetForm() {
+    resetInputs();
+    resetButtons();
+    resetCustom();
+    resetBtn.disabled = true;
+}
+
+function calculateAmount() {
+
 }
